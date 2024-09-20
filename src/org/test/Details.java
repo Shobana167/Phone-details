39d211ffa97f07569ea8eb9b3bb5198cacdf6f44
Shobana167 <shobha167@gmail.com>
2024-09-20 10:37:58 +0530

    details added

---
+++ src/org/test/Details.java
@@ -0,0 +1,35 @@
+package org.test;
+
+public class Details {
+
+	public static void main(String[] args) {
+	byte age = 31;
+	System.out.println("Employee age is : " +age);
+	short empid =5656;
+	System.out.println("Employee empid is : " +empid);
+	int rollNo = 31456776;
+	System.out.println("Employee rollno is : " +rollNo);
+	long phNo = 9876589999787675l;
+	System.out.println("Employee phno is : " + phNo);
+	float salary = 31000.345566f;
+	System.out.println("Employee salary is : " +salary);
+	double preSalary = 3109.4566;
+	System.out.println("Employee presalary is : " +preSalary);
+	char gender = 'F';
+	System.out.println("Employee gender is : " + gender);
+	String name = "Greens";
+	System.out.println("Employee name is :" + name);
+	boolean maritalStatus = true;
+	System.out.println("Employee marital status is : " + maritalStatus);
+
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
